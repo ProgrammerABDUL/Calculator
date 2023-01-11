7f40095545bfaf7c7af81fcd18bcf0821e81c384
Abdul Rehman Jiwani <112803678+ProgrammerABDUL@users.noreply.github.com>
2023-01-11 14:12:58 +0500

    Add files via upload

---
+++ sound.js
@@ -0,0 +1,15 @@
+// Sound Functions
+function Clearsound() {
+    const sound = new Audio('Clear-sound.mp3')
+    sound.play()
+}
+
+function Keysound() {
+    const key = new Audio('Key-sound.wav')
+    key.play()
+}
+
+function Operatorsound() {
+    const operator = new Audio('Operator-sound.mp3')
+    operator.play()
+}
